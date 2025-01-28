7f8800c9e5109c4c35eecff81332f34a6c58d5fd
Igor Bugaytsov <i.bugaytsov@mail.ru>
2025-01-28 14:13:05 +0300

    Выполнение пункта 3

--- main.js
+++ main.js
@@ -5,56 +5,59 @@ const todoCompleted = document.querySelector('.todo-completed')
 
 const data = []
 
-const reg = function() {
+const reg = function () {
     todoList.innerHTML = ''
     todoCompleted.innerHTML = ''
-    data.forEach(function(item, index) {
-        
-       const li = document.createElement('li')
+    data.forEach(function (item, index) {
+
+        const li = document.createElement('li')
         li.classList.add('todo-item')
         li.innerHTML = '<span class="text-todo">' + item.text + '</span>' +
-        '<div class="todo-buttons">' +
-		'<button class="todo-remove"></button>' +
-		'<button class="todo-complete"></button>' +
-		'</div>'
+            '<div class="todo-buttons">' +
+            '<button class="todo-remove"></button>' +
+            '<button class="todo-complete"></button>' +
+            '</div>'
 
-        if(item.completed) {
+        if (item.completed) {
             todoCompleted.append(li)
         } else {
             todoList.append(li)
         }
 
         const check = li.querySelector('.todo-complete')
-        check.addEventListener('click', function() {
+        check.addEventListener('click', function () {
             item.completed = !item.completed
             reg()
         })
 
         const del = li.querySelector('.todo-remove')
-        del.addEventListener('click', function() {
-            
-            console.log(data);
-            
+        del.addEventListener('click', function () {
+            const itemRemove = data.indexOf(item)
+            data.splice(itemRemove, 1)
+            reg()
+
+            console.log(itemRemove);
+
         })
-        
+
     })
 }
 
-todoControl.addEventListener('submit', function(event) {
+todoControl.addEventListener('submit', function (event) {
     event.preventDefault()
 
     const newObj = {
         text: headerInput.value,
         compledted: false
     }
-    if(headerInput.value === '') {
+    if (headerInput.value === '') {
         alert('Введите задачу');
-        
+
     } else {
         data.push(newObj)
-        
+
     }
-    
+
     headerInput.value = ''
     reg()
 })
